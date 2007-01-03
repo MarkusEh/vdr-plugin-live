db6b5bafdd8c1aa1fe50a9ad9928ef0070d47028
Sascha Volkenandt <sascha (at) akv-soft (dot) de>
2007-01-03 16:01:28 +0000

    - added missing terminating 0 entry

--- i18n.cpp
+++ i18n.cpp
@@ -3,30 +3,31 @@
 namespace vdrlive {
 
 const tI18nPhrase Phrases[] = {
-  // The name of the language
-  /*
-  { "English",
-    "Deutsch",
-    "Slovenski",
-    "Italiano",
-    "Nederlands",
-    "Português",
-    "Français",
-    "Norsk",
-    "suomi", // Finnish (this is not a typo - it's really lowercase!)
-    "Polski",
-    "Español",
-    "ÅëëçíéêÜ", // Greek
-    "Svenska",
-    "Românã",
-    "Magyar",
-    "Català",
-    "ÀãááÚØÙ", // Russian
-    "Hrvatski",
-    "Eesti",
-    "Dansk",
-    "Èesky", //Czech
-  }, */
+	// The name of the language
+	/*
+	{ "English",
+	  "Deutsch",
+	  "Slovenski",
+	  "Italiano",
+	  "Nederlands",
+	  "Português",
+	  "Français",
+	  "Norsk",
+	  "suomi", // Finnish (this is not a typo - it's really lowercase!)
+	  "Polski",
+	  "Español",
+	  "ÅëëçíéêÜ", // Greek
+	  "Svenska",
+	  "Românã",
+	  "Magyar",
+	  "Català",
+	  "ÀãááÚØÙ", // Russian
+	  "Hrvatski",
+	  "Eesti",
+	  "Dansk",
+	  "Èesky", //Czech
+	}, */
+	{ 0 },
 };
 
 } // namespace vdrlive
