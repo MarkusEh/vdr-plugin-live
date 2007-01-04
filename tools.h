8db65a15e220b33371b1d3c502a05bf308316db4
Sascha Volkenandt <sascha (at) akv-soft (dot) de>
2007-01-04 14:18:54 +0000

    - added read lock objekt for channels

--- tools.h
+++ tools.h
@@ -3,10 +3,25 @@
 
 #include <ctime>
 #include <string>
+#include <vdr/thread.h>
 
 namespace vdrlive {
 
 std::string FormatDateTime( char const* format, time_t time );
+
+class ReadLock
+{
+public:
+	ReadLock( cRwLock& lock, int timeout = 100 ): m_lock( lock ), m_locked( false ) { if ( m_lock.Lock( false, timeout ) ) m_locked = true; }
+	~ReadLock() { if ( m_locked ) m_lock.Unlock(); }
+
+	operator bool() { return m_locked; }
+	bool operator!() { return !m_locked; }
+
+private:
+	cRwLock& m_lock;
+	bool m_locked;
+};
 
 } // namespace vdrlive
 
