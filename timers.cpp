93f1c156edfd067627cd969ca275f38d3b319069
Sascha Volkenandt <sascha (at) akv-soft (dot) de>
2007-01-05 17:52:00 +0000

    - moved SortedTimers to plugin and added methods to interface from website - added function to update internal timers from main thread

--- timers.cpp
+++ timers.cpp
@@ -1,9 +1,51 @@
+#include <sstream>
 #include "timers.h"
+
+static bool operator<( cTimer const& left, cTimer const& right )
+{
+	return left.Compare( right ) < 0;
+}
 
 namespace vdrlive {
 
+using namespace std;
+
+SortedTimers::SortedTimers():
+		m_state( 0 )
+{
+	ReloadTimers( true );
+}
+
+string SortedTimers::GetTimerId( cTimer const& timer )
+{
+	ostringstream builder;
+	builder << *timer.Channel()->GetChannelID().ToString() << ":" << timer.WeekDays() << ":"
+			<< timer.Day() << ":" << timer.Start() << ":" << timer.Stop();
+	return builder.str();
+}
+
+void SortedTimers::ReloadTimers( bool initial )
+{
+	if ( !Timers.Modified( m_state ) && !initial )
+		return;
+
+	dsyslog("reloading timers");
+
+	clear();
+	for ( cTimer* timer = Timers.First(); timer != 0; timer = Timers.Next( timer ) ) {
+		push_back( *timer );
+	}
+	sort();
+}
+
 TimerManager::TimerManager()
 {
+}
+
+void TimerManager::DoPendingWork()
+{
+	cMutexLock lock( this );
+	m_timers.ReloadTimers();
 }
 
 } // namespace vdrlive
--- timers.h
+++ timers.h
@@ -2,22 +2,55 @@
 #define VDR_LIVE_TIMERS_H
 
 #include <list>
+#include <string>
 #include <vdr/timers.h>
+#include <vdr/thread.h>
 #include "live.h"
 
 namespace vdrlive {
 
 class Plugin;
 
-class TimerManager
+class SortedTimersInterface: public std::list< cTimer >
+{
+public:
+	virtual ~SortedTimersInterface() {}
+
+	virtual std::string GetTimerId( cTimer const& timer ) = 0;
+};
+
+class SortedTimers: public SortedTimersInterface
+{
+	friend class TimerManager;
+
+public:
+	virtual std::string GetTimerId( cTimer const& timer );
+	
+private:
+	SortedTimers();
+	SortedTimers( SortedTimers const& );
+
+	int m_state;
+	int m_refs;
+	
+	void ReloadTimers( bool initial = false );
+};
+
+class TimerManager: public cMutex
 {
 	friend TimerManager& Plugin::GetLiveTimerManager();
+
+public:
+	SortedTimersInterface& GetTimers() { return m_timers; }
+
+	// may only be called from Plugin::MainThreadHook
+	void DoPendingWork();
 
 private:
 	TimerManager();
 	TimerManager( TimerManager const& );
 
-	
+	SortedTimers m_timers;
 };
 
 inline TimerManager& LiveTimerManager()
