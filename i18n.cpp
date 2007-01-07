336140c98b57fa88798564c421e5d2da50ce0e19
Dieter Hametner <dh (plus) vdr (at) gekrumbel (dot) de>
2007-01-07 00:46:21 +0000

    added error handling framework to recordings.ecpp

--- i18n.cpp
+++ i18n.cpp
@@ -201,6 +201,28 @@ const tI18nPhrase Phrases[] = {
 	  "", // Dansk
 	  "", //Czech
 	},
+	{ "Save", // English
+	  "Speichern", // Deutsch
+	  "", // Slovenski
+	  "", // Italiano
+	  "", // Nederlands
+	  "", // Português
+	  "", // Français
+	  "", // Norsk
+	  "", // Finnish
+	  "", // Polski
+	  "", // Español
+	  "", // Greek
+	  "", // Svenska
+	  "", // Românã
+	  "", // Magyar
+	  "", // Català
+	  "", // Russian
+	  "", // Hrvatski
+	  "", // Eesti
+	  "", // Dansk
+	  "", //Czech
+	},
 	{ "Menu", // English
 	  "Menü", // Deutsch
 	  "", // Slovenski
