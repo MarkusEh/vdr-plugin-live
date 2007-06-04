c1c33bd6daf2d27494f23d9867f1c181bfa694e9
Christian Wieninger <cwieninger (at) gmx (dot) de>
2007-06-04 16:40:15 +0000

    - check for login/password before saving setup (#351)

--- i18n.cpp
+++ i18n.cpp
@@ -4143,6 +4143,28 @@ const tI18nPhrase Phrases[] = {
       "", // Dansk
       "", // Czech
       },
+      { "Please set login and password!", // English
+      "Bitte Login und Passwort angeben!", // Deutsch
+      "", // Slovenski
+      "", // Italiano
+      "", // Nederlands
+      "", // Português
+      "", // Français
+      "", // Norsk
+      "", // Finnish
+      "", // Polski
+      "", // Español
+      "", // Greek
+      "", // Svenska
+      "", // Românã
+      "", // Magyar
+      "", // Català
+      "", // Russian
+      "", // Hrvatski
+      "", // Eesti
+      "", // Dansk
+      "", // Czech
+      },
     /*
       { "", // English
       "", // Deutsch
