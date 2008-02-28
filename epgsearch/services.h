01d8689f9260fb063de7712681b243fb48ef513a
Christian Wieninger <cwieninger (at) gmx (dot) de>
2008-02-28 20:29:47 +0100

    changes for new service interface version in epgsearch

--- epgsearch/services.h
+++ epgsearch/services.h
@@ -1,3 +1,26 @@
+/*
+Copyright (C) 2004-2007 Christian Wieninger
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+Or, point your browser to http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+The author can be reached at cwieninger@gmx.de
+
+The project's page is at http://winni.vdr-developer.org/epgsearch
+*/
+
 #ifndef EPGSEARCHSERVICES_INC
 #define EPGSEARCHSERVICES_INC
 
@@ -129,16 +152,28 @@ class cServiceHandler
    virtual std::string ReadSetupValue(const std::string& entry) = 0;
    // Write a setup value
    virtual bool WriteSetupValue(const std::string& entry, const std::string& value) = 0;
-   // Get timer conflicts
-   virtual std::list<std::string> TimerConflictList(bool relOnly=false) = 0;    
-   // Check if a conflict check is advised
-   virtual bool IsConflictCheckAdvised() = 0;    
 };
 
 struct Epgsearch_services_v1_0
 {
 // in/out
       std::auto_ptr<cServiceHandler> handler;
+};
+
+// Data structures for service "Epgsearch-services-v1.0"
+class cServiceHandler_v1_1 : public cServiceHandler
+{
+  public:
+   // Get timer conflicts
+   virtual std::list<std::string> TimerConflictList(bool relOnly=false) = 0;    
+   // Check if a conflict check is advised
+   virtual bool IsConflictCheckAdvised() = 0;    
+};
+
+struct Epgsearch_services_v1_1
+{
+// in/out
+      std::auto_ptr<cServiceHandler_v1_1> handler;
 };
 
 #endif
