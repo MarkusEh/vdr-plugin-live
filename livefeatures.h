78c2040f1265845f1e0919c4107426b7cc1cf684
Christian Wieninger <cwieninger (at) gmx (dot) de>
2008-04-07 18:43:39 +0200

    required streamdev version: "none" to "?"

--- livefeatures.h
+++ livefeatures.h
@@ -66,7 +66,7 @@ namespace features
 	struct streamdev_server
 	{
 		static const char* Plugin() { return "streamdev-server"; }
-		static const char* MinVersion() { return "none"; }
+		static const char* MinVersion() { return "?"; }
 	};
 } // namespace features
 
