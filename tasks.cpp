c3ecf9fabb674bc154f5a08cdd62e74e2b5d0e36
Christian Ruppert <idl0r@gentoo.org>
2012-06-04 01:42:31 +0200

    Compile fix for >=vdr-1.7.28

--- tasks.cpp
+++ tasks.cpp
@@ -60,9 +60,15 @@ void PlayRecordingTask::Action()
 
 	const char *current = NowReplaying();
 	if (!current || (0 != strcmp(current, recording->FileName()))) {
+#if VDRVERSNUM >= 10728
+		cReplayControl::SetRecording( 0 );
+		cControl::Shutdown();
+		cReplayControl::SetRecording( recording->FileName() );
+#else
 		cReplayControl::SetRecording( 0, 0 );
 		cControl::Shutdown();
 		cReplayControl::SetRecording( recording->FileName(), recording->Title() );
+#endif
 		cControl::Launch( new cReplayControl );
 		cControl::Attach();
 	}
@@ -122,7 +128,11 @@ void StopRecordingTask::Action()
 		return;
 	}
 
+#if VDRVERSNUM >= 10728
+	cReplayControl::SetRecording( 0 );
+#else
 	cReplayControl::SetRecording( 0, 0 );
+#endif
 	cControl::Shutdown();
 }
 
