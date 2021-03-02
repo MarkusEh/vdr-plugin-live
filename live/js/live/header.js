8ebc61d787d2687319f42d5a9393d0c3e31167f4
Markus Ehrnsperger <markus ehrnsperger>
2021-03-02 21:53:29 +0100

    live/js/live/header.js

---
+++ live/js/live/header.js
@@ -0,0 +1,21 @@
+function adjustHeader()
+{
+	// exchange body top margin by equally-sized header-padding element
+	var padding = document.getElementById( "padding" );
+	if( padding.style.height == "" )
+	{
+		var bodyStyles = ( window.getComputedStyle )? getComputedStyle( document.body, null ) : document.body.currentStyle;
+		padding.style.height = bodyStyles.marginTop;
+		padding.style.backgroundColor = ( window.bgColor )? window.bgColor : "white";
+		padding.style.display = "";
+		document.body.style.marginTop = "0px";
+	}
+	// expand underlay to header's height
+	var header = document.getElementById( "header" );
+	var underlay = document.getElementById( "underlay" );
+	underlay.style.height = header.offsetHeight+ "px";
+	underlay.style.display = "";
+	header.style.position = "fixed";
+	return;
+}
+
