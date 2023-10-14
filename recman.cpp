90b680ea928b0a8ec351120a0251ac203b9e1a13
Markus Ehrnsperger <>
2023-10-14 19:46:09 +0200

    newFolder

--- recman.cpp
+++ recman.cpp
@@ -97,6 +97,12 @@ namespace vdrlive {
 		if (!recording)
 			return false;
 
+// Check for injections that try to escape from the video dir.
+		if (name.compare(0, 3, "../") == 0 || name.find("/..") != std::string::npos) {
+			esyslog("live: renaming failed: new name invalid \"%s\"", name.c_str());
+			return false;
+		}
+
 		std::string oldname = recording->FileName();
 		size_t found = oldname.find_last_of("/");
 
