9967f1d6757a4f4855a6b07abf526258838dd5ac
Markus Ehrnsperger <>
2024-11-03 16:58:27 +0100

    timer_directories2

--- setup.h
+++ setup.h
@@ -12,8 +12,8 @@
 #include "stringhelpers.h"
 #include <vdr/menuitems.h>
 
-#define LIVEVERSION "3.3.8"
-#define LIVEVERSNUM 30308
+#define LIVEVERSION "3.3.9"
+#define LIVEVERSNUM 30309
 #define LIVESUMMARY trNOOP("Live Interactive VDR Environment")
 
 namespace vdrlive {
